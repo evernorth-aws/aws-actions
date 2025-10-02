97e1701b90689048f1e596e20661495ec0eb0aab
evernorth-aws <shankaruday2025@gmail.com>
2025-10-02 12:55:43 -0500

    Update providers.tf

--- infra-ec2/providers.tf
+++ infra-ec2/providers.tf
@@ -7,12 +7,6 @@ terraform {
   }
 }
 
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-2"
-}
-
 provider "aws" {
   region = var.aws_region
 }
