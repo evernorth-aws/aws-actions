163a37c8e48240244bec9700266646b8fca28780
evernorth-aws <shankaruday2025@gmail.com>
2025-10-02 12:56:27 -0500

    Update ec2.tf

--- infra-ec2/ec2.tf
+++ infra-ec2/ec2.tf
@@ -14,9 +14,20 @@ data "aws_subnets" "default" {
   }
 }
 
-# Latest Amazon Linux 2 AMI via SSM Parameter (stable and region-aware)
-data "aws_ssm_parameter" "amzn2_ami" {
-  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+# Latest Amazon Linux 2 AMI via EC2 AMI lookup (no SSM permission needed)
+data "aws_ami" "amzn2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
 }
 
 resource "aws_security_group" "demo" {
@@ -37,8 +48,8 @@ resource "aws_security_group" "demo" {
 }
 
 resource "aws_instance" "demo" {
-  ami                         = data.aws_ssm_parameter.amzn2_ami.value
-  instance_type               = "t2.micro"
+  ami                         = data.aws_ami.amzn2.id
+  instance_type               = var.instance_type
   subnet_id                   = data.aws_subnets.default.ids[0]
   vpc_security_group_ids      = [aws_security_group.demo.id]
   associate_public_ip_address = true
